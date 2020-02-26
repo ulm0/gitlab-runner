edcac1493ec271300c320f05c021baf0175f4ba2
Pierre Ugaz <ulm0@innersea.xyz>
2020-02-26 04:08:22 -0300

    feat: module
    Signed-off-by: Pierre Ugaz <ulm0@innersea.xyz>

--- ci/scway/arm/main.tf
+++ ci/scway/arm/main.tf
@@ -12,47 +12,9 @@ provider "scaleway" {
   zone   = "fr-par-1"
 }
 
-resource "tls_private_key" "runner_key" {
-  algorithm = "RSA"
-  rsa_bits  = "4096"
-}
-
-data "scaleway_image" "docker" {
-  architecture = "arm"
-  name         = "Docker"
-  most_recent  = true
-}
-
-resource "scaleway_account_ssh_key" "runner_key" {
-  name       = "runner_key"
-  public_key = tls_private_key.runner_key.public_key_openssh
-}
-
-data "scaleway_bootscript" "bootscript" {
-  architecture = "arm"
-  name_filter  = "4.9 latest"
-}
-
-
-resource "scaleway_server" "runner" {
-  name                = "runner"
-  image               = data.scaleway_image.docker.id
-  dynamic_ip_required = true
-  type                = "C1"
-  boot_type           = "bootscript"
-  bootscript          = data.scaleway_bootscript.bootscript.id
-
-  connection {
-    type        = "ssh"
-    user        = "root"
-    host        = self.public_ip
-    private_key = tls_private_key.runner_key.private_key_pem
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "docker info",
-      "uname -a",
-    ]
-  }
+module "arm" {
+  source = "../module/runner"
+  arch = "arm"
+  bootscript_name_filter = "4.9 latest"
+  server_type = "C1"
 }
--- ci/scway/arm64/main.tf
+++ ci/scway/arm64/main.tf
@@ -12,46 +12,9 @@ provider "scaleway" {
   zone   = "nl-ams-1"
 }
 
-resource "tls_private_key" "runner_key" {
-  algorithm = "RSA"
-  rsa_bits  = "4096"
-}
-
-data "scaleway_image" "docker" {
-  architecture = "arm64"
-  name         = "Docker"
-  most_recent  = true
-}
-
-resource "scaleway_account_ssh_key" "runner_key" {
-  name       = "runner_key"
-  public_key = tls_private_key.runner_key.public_key_openssh
-}
-
-data "scaleway_bootscript" "bootscript" {
-  architecture = "arm64"
-  name_filter  = "4.19 latest"
-}
-
-resource "scaleway_server" "runner" {
-  name                = "runner"
-  image               = data.scaleway_image.docker.id
-  dynamic_ip_required = true
-  type                = "ARM64-2GB"
-  boot_type           = "bootscript"
-  bootscript          = data.scaleway_bootscript.bootscript.id
-
-  connection {
-    type        = "ssh"
-    user        = "root"
-    host        = self.public_ip
-    private_key = tls_private_key.runner_key.private_key_pem
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "docker info",
-      "uname -a",
-    ]
-  }
+module "arm64" {
+  source = "../module/runner"
+  arch = "arm64"
+  bootscript_name_filter = "4.19 latest"
+  server_type = "ARM64-2GB"
 }
